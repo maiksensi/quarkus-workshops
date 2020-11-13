4c2b4efb93eb5a28526b488d2e1f6ac63ea62e98
Maik Figura <maik.figura@codecentric.de>
2020-11-13 15:50:29 +0100

    Update quarkus to 1.9.2

--- rest-hero/bootstrap.sh
+++ rest-hero/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 cd quarkus-workshop-super-heroes/super-heroes
-mvn io.quarkus:quarkus-maven-plugin:1.7.0.Final:create \
+mvn io.quarkus:quarkus-maven-plugin:1.9.2.Final:create \
     -DprojectGroupId=io.quarkus.workshop.super-heroes \
     -DprojectArtifactId=rest-hero \
     -DclassName="io.quarkus.workshop.superheroes.hero.HeroResource" \
